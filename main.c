aa9ddfcbb6bd3d6d93244ea12072b6d5ff317f8e
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-01-24 16:20:21 +0000

    login to discord bot and register ready event

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
+#include <assert.h>
+#include <concord/discord.h>
+#include <concord/log.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void on_ready(struct discord *client, const struct discord_ready *event) {
+  log_info("Logged in as %s!", event->user->username);
+}
 
 int main(int argc, char *argv[]) {
-  printf("%s\n", "Hello World!");
-  return 0;
+  // config file name
+  const char *config_file = argc > 1 ? argv[1] : "../config.json";
+
+  ccord_global_init();
+  // login to discord
+  struct discord *client = discord_config_init(config_file);
+  assert(NULL != client && "Couldn't initialize client");
+
+  // register ready event
+  discord_set_on_ready(client, &on_ready);
+
+  // run the client
+  discord_run(client);
+
+  // release memory
+  discord_cleanup(client);
+  ccord_global_cleanup();
 }
