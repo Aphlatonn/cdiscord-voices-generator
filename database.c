a496864f127cf3defac1089d6d42c1f007b68676
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-01-24 21:53:52 +0000

    Add db_guild_insert func for inserting raws to guilds table

--- database.c
+++ database.c
@@ -1,3 +1,4 @@
+#include "database.h"
 #include <sqlite3.h>
 #include <stdio.h>
 
@@ -27,9 +28,9 @@ int create_tables(void) {
       "CREATE TABLE IF NOT EXISTS guilds (" // guilds table
       "id             INTEGER PRIMARY KEY,"
       "prefix         TEXT,"
-      "voice          TEXT,"
-      "reject_voice   TEXT,"
-      "logs_channel   TEXT);"
+      "voice          INTEGER,"
+      "reject_voice   INTEGER,"
+      "logs_channel   INTEGER);"
       "CREATE TABLE IF NOT EXISTS temp_voices (" // tempvoices table
       "id             INTEGER PRIMARY KEY,"
       "guild_id       INTEGER,"
@@ -51,4 +52,38 @@ void close_database(void) {
     sqlite3_close(db);
     db = 0;
   }
+}
+
+// insert raw into guilds table
+int db_guild_insert(guild_data data) {
+  const char *sql =
+      "INSERT INTO guilds (id, prefix, voice, reject_voice, logs_channel) "
+      "VALUES (?, ?, ?, ?, ?);";
+  sqlite3_stmt *stmt;
+  int rc;
+
+  rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
+
+  // bind values to the placeholders
+  sqlite3_bind_int64(stmt, 1, data.id);
+  sqlite3_bind_text(stmt, 2, data.prefix, -1, SQLITE_STATIC);
+  sqlite3_bind_int64(stmt, 3, data.voice);
+  sqlite3_bind_int64(stmt, 4, data.reject_voice);
+  sqlite3_bind_int64(stmt, 5, data.logs_channel);
+
+  // Execute the statement
+  rc = sqlite3_step(stmt);
+  if (rc != SQLITE_DONE) {
+    fprintf(stderr, "Execution failed: %s\n", sqlite3_errmsg(db));
+    sqlite3_finalize(stmt);
+    return -1;
+  }
+
+  // Finalize the statement to release resources
+  sqlite3_finalize(stmt);
+  return 0;
 }
--- database.h
+++ database.h
@@ -1,3 +1,17 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <concord/types.h>
+#include <sqlite3.h>
+
+typedef struct {
+  u64snowflake id;
+  const char *prefix;
+  u64snowflake voice;
+  u64snowflake reject_voice;
+  u64snowflake logs_channel;
+} guild_data;
+
 // initialize databse func
 int initialize_database(const char *database_file);
 
@@ -6,3 +20,8 @@ int create_tables(void);
 
 // close the database
 void close_database(void);
+
+// insert raw into guilds table
+int db_guild_insert(guild_data data);
+
+#endif // DATABASE_H
