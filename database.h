af990900f280ecc1612faa4e8b8608199c725f07
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-01-24 19:43:20 +0000

    Add database.c header file

---
+++ database.h
@@ -0,0 +1,8 @@
+// initialize databse func
+int initialize_database(const char *database_file);
+
+// create tables if not exist
+int create_tables(void);
+
+// close the database
+void close_database(void);
