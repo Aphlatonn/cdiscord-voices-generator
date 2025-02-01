4160ad0122b639cf9d6c9125b314874595c67ca1
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-01 22:55:32 +0000

    Add split_at_first_delim func, splits a string at the first occurrence of a delimiter character

--- utils.c
+++ utils.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 int starts_with(char *str, char *prefix) {
   while (*prefix) {
     if (*str != *prefix) {
@@ -7,4 +9,20 @@ int starts_with(char *str, char *prefix) {
     prefix++;
   }
   return 1;
+}
+
+void split_at_first_delim(const char *str, char delim, char *dist1,
+                          char *dist2) {
+  const char *str_temp_p = str;
+
+  while (*str_temp_p) {
+    if (*str_temp_p == delim) {
+      strncpy(dist1, str, str_temp_p - str);
+      dist1[str_temp_p - str] = '\0';
+
+      strcpy(dist2, str_temp_p + 1);
+      break;
+    }
+    str_temp_p++;
+  }
 }
