e351650478ba62e3a91ac25741193c2109f2ffff
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-01 16:08:55 +0000

    Add command handling, error cleanup, and help command

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include "database.h"
 #include <assert.h>
 #include <concord/discord.h>
 #include <concord/log.h>
@@ -6,8 +5,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "commands.h"
+#include "database.h"
+#include "utils.h"
+
 void on_ready(struct discord *client, const struct discord_ready *event) {
   log_info("Logged in as %s!", event->user->username);
+}
+
+void on_message_create(struct discord *client,
+                       const struct discord_message *event) {
+  // return if the message author is a bot
+  if (event->author->bot)
+    return;
+
+  const char *prefix = "!";
+  if (!starts_with(event->content, prefix)) {
+    return;
+  }
+
+  // find the command
+  // TODO: replace 1 with the prefix lenght
+  const command *cmd = get_command(event->content + 1);
+
+  // return if the commands was not found
+  if (cmd == 0)
+    return;
+
+  // run the command
+  cmd->run(client, event);
 }
 
 int main(int argc, char *argv[]) {
@@ -19,37 +45,39 @@ int main(int argc, char *argv[]) {
   struct discord *client = discord_config_init(config_file);
   assert(NULL != client && "Couldn't initialize client");
 
+  int returnStatusCode = 0;
+
   // initialize the database
   if (initialize_database("./database.db") != 0) {
-    discord_cleanup(client);
-    ccord_global_cleanup();
-    return -1;
+    returnStatusCode = -1;
+    goto cleanup;
   } else {
     log_info("Database has been initialized");
   }
 
   // create the tables
   if (create_tables() != 0) {
-    discord_cleanup(client);
-    ccord_global_cleanup();
-    return -1;
+    returnStatusCode = -1;
+    goto cleanup;
   } else {
     log_info("Tables has been created");
   }
 
-  // add voice state update intents
+  // add voice state update intent and message intent
   discord_add_intents(client, DISCORD_GATEWAY_VOICE_STATE_UPDATE);
+  discord_add_intents(client, DISCORD_GATEWAY_MESSAGE_CONTENT);
 
-  // register ready event
+  // register ready and message create events
   discord_set_on_ready(client, &on_ready);
+  discord_set_on_message_create(client, &on_message_create);
 
   // run the client
   discord_run(client);
 
   // release memory
+cleanup:
   discord_cleanup(client);
   ccord_global_cleanup();
   close_database();
-
-  return 0;
+  return returnStatusCode;
 }
