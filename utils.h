7a79a4e2fe66d5a2104042419292d78db8c0f7c5
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-01 22:57:14 +0000

    Add declaration for split_at_first_delim in header file

--- utils.h
+++ utils.h
@@ -4,4 +4,8 @@
 // check if str start with prefix
 int starts_with(const char *str, const char *prefix);
 
+// split at the first delim
+void split_at_first_delim(const char *str, char delim, char *dist1,
+                          char *dist2);
+
 #endif // UTILS_H
