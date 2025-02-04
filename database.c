537bc6648d10e6bcabbc560d204289edfad20ae4
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-04 20:23:43 +0000

    Refactor database logic to work well with multithreaded environment

--- database.c
+++ database.c
@@ -1,90 +1,146 @@
 #include <sqlite3.h>
+#include <stdint.h>
 #include <stdio.h>
 
 #include "database.h"
 
-static sqlite3 *db = 0;
+static const char *DATABASE_FILE = 0;
 
-// initialize databse func
 int initialize_database(const char *database_file) {
-  // Check if the database has already been initialized
-  if (db != 0) {
-    return 0;
-  }
-
-  // open the sqlite database
+  DATABASE_FILE = database_file;
+  sqlite3 *db;
   int rc = sqlite3_open(database_file, &db);
   if (rc) {
     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
     return -1;
   }
 
-  return 0;
-}
-
-// create tables if not exist
-int create_tables(void) {
-  // sql
   const char *sql =
-      "CREATE TABLE IF NOT EXISTS guilds (" // guilds table
-      "id             INTEGER PRIMARY KEY,"
-      "prefix         TEXT,"
-      "voice          INTEGER,"
-      "reject_voice   INTEGER,"
-      "logs_channel   INTEGER);"
-      "CREATE TABLE IF NOT EXISTS temp_voices (" // tempvoices table
-      "id             INTEGER PRIMARY KEY,"
-      "guild_id       INTEGER,"
-      "owner_id       INTEGER)";
+      // guilds table
+      "CREATE TABLE IF NOT EXISTS guilds ("
+      "id INTEGER PRIMARY KEY, voice INTEGER, "
+      "reject_voice INTEGER, logs_channel INTEGER);"
+      // temp_voices table
+      "CREATE TABLE IF NOT EXISTS temp_voices ("
+      "id INTEGER PRIMARY KEY, guild_id INTEGER, owner_id INTEGER);";
 
-  // create tables
-  int rc = sqlite3_exec(db, sql, 0, 0, 0);
+  char *errmsg = 0;
+  rc = sqlite3_exec(db, sql, 0, 0, &errmsg);
   if (rc != SQLITE_OK) {
-    fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+    fprintf(stderr, "SQL error: %s\n", errmsg);
+    sqlite3_free(errmsg);
+    sqlite3_close(db);
     return -1;
   }
 
+  sqlite3_close(db);
   return 0;
 }
 
-// close the database
-void close_database(void) {
-  if (db != 0) {
-    sqlite3_close(db);
-    db = 0;
-  }
-}
-
-// insert raw into guilds table
 int db_guild_insert(guild_data data) {
-  const char *sql =
-      "INSERT INTO guilds (id, prefix, voice, reject_voice, logs_channel) "
-      "VALUES (?, ?, ?, ?, ?);";
+  sqlite3 *db;
+  int rc = sqlite3_open(DATABASE_FILE, &db);
+  if (rc) {
+    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
+
+  const char *sql = "INSERT INTO guilds (id, voice, reject_voice, "
+                    "logs_channel) VALUES (?, ?, ?, ?);";
   sqlite3_stmt *stmt;
-  int rc;
 
   rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
   if (rc != SQLITE_OK) {
     fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
     return -1;
   }
 
-  // bind values to the placeholders
   sqlite3_bind_int64(stmt, 1, data.id);
-  sqlite3_bind_text(stmt, 2, data.prefix, -1, SQLITE_STATIC);
-  sqlite3_bind_int64(stmt, 3, data.voice);
-  sqlite3_bind_int64(stmt, 4, data.reject_voice);
-  sqlite3_bind_int64(stmt, 5, data.logs_channel);
+  sqlite3_bind_int64(stmt, 2, data.voice);
+  sqlite3_bind_int64(stmt, 3, data.reject_voice);
+  sqlite3_bind_int64(stmt, 4, data.logs_channel);
 
-  // Execute the statement
   rc = sqlite3_step(stmt);
+  sqlite3_finalize(stmt);
+  sqlite3_close(db);
+
   if (rc != SQLITE_DONE) {
     fprintf(stderr, "Execution failed: %s\n", sqlite3_errmsg(db));
-    sqlite3_finalize(stmt);
     return -1;
   }
 
-  // Finalize the statement to release resources
+  return 0;
+}
+
+int db_guild_read(uint64_t guild_id, guild_data *dest) {
+  sqlite3 *db;
+  int rc = sqlite3_open(DATABASE_FILE, &db);
+  if (rc) {
+    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
+
+  const char *sql = "SELECT id, voice, reject_voice, logs_channel FROM "
+                    "guilds WHERE id = ?";
+  sqlite3_stmt *stmt;
+  rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return -1;
+  }
+
+  sqlite3_bind_int64(stmt, 1, guild_id);
+  rc = sqlite3_step(stmt);
+
+  if (rc == SQLITE_ROW) {
+    dest->id = guild_id;
+    dest->voice = sqlite3_column_int64(stmt, 1);
+    dest->reject_voice = sqlite3_column_int64(stmt, 2);
+    dest->logs_channel = sqlite3_column_int64(stmt, 3);
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+    return 0; // row found
+  } else if (rc == SQLITE_DONE) {
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+    return 1; // row not found
+  } else {
+    fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+    return -1; // err
+  }
+}
+
+int db_guild_delete(uint64_t guild_id) {
+  sqlite3 *db;
+  int rc = sqlite3_open(DATABASE_FILE, &db);
+  if (rc) {
+    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
+
+  const char *sql = "DELETE FROM guilds WHERE id = ?";
+  sqlite3_stmt *stmt;
+  rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return -1;
+  }
+
+  sqlite3_bind_int64(stmt, 1, guild_id);
+  rc = sqlite3_step(stmt);
   sqlite3_finalize(stmt);
+  sqlite3_close(db);
+
+  if (rc != SQLITE_DONE) {
+    fprintf(stderr, "Failed to delete: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
+
   return 0;
 }
--- database.h
+++ database.h
@@ -1,27 +1,26 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
-#include <concord/types.h>
 #include <sqlite3.h>
+#include <stdint.h>
 
 typedef struct {
-  u64snowflake id;
-  const char *prefix;
-  u64snowflake voice;
-  u64snowflake reject_voice;
-  u64snowflake logs_channel;
+  uint64_t id;
+  uint64_t voice;
+  uint64_t reject_voice;
+  uint64_t logs_channel;
 } guild_data;
 
 // initialize databse func
 int initialize_database(const char *database_file);
 
-// create tables if not exist
-int create_tables(void);
-
-// close the database
-void close_database(void);
-
 // insert raw into guilds table
 int db_guild_insert(guild_data data);
+
+// read raw from guilds table
+int db_guild_read(uint64_t guild_id, guild_data *dest);
+
+// delete raw from guilds table
+int db_guild_delete(uint64_t guild_id);
 
 #endif // DATABASE_H
